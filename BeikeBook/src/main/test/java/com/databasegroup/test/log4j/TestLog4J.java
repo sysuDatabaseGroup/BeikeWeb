daeb2b333ed29e580bb6985b1ca16712b684cc8e
seevenun <i.7un@qq.com>
2016-10-23 10:46:49 +0800

    update

---
+++ BeikeBook/src/main/test/java/com/databasegroup/test/log4j/TestLog4J.java
@@ -0,0 +1,17 @@
+package com.databasegroup.test.log4j;
+
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+
+public class TestLog4J {
+	
+	private static Logger logger = Logger.getLogger(TestLog4J.class);  
+	
+	public static void main(String[] args) {
+		logger.debug("debug information");
+		logger.info("info information");
+		logger.error("error information");
+	}
+}
